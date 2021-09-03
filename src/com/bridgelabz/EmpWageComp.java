2909dca31f3ba806daf81797485daf0a8e2ffeeb
RajendraGund <rajendragund2015@gmail.com>
2021-09-04 00:49:06 +0530

    Added : Welcome message for User

---
+++ src/com/bridgelabz/EmpWageComp.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmpWageComp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wage Computation Program !!");
+    }
+}
