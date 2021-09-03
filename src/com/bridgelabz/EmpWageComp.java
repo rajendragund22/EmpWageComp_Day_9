604eb18946fcc42c8bd003d81b7631d576602c9e
RajendraGund <rajendragund2015@gmail.com>
2021-09-04 02:15:01 +0530

    Added : Program to Refactor the code

--- src/com/bridgelabz/EmpWageComp.java
+++ src/com/bridgelabz/EmpWageComp.java
@@ -12,6 +12,9 @@ public class EmpWageComp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation Program !!");
+        EmployeeWageComp();
+    }
+    public static void EmployeeWageComp(){
         int workingDay = 20;
         int empHrs = 0;
         int empwage = 0;
