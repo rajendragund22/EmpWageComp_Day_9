3055309aaccf92c727f1f8cc2b40534c729ba7c5
RajendraGund <rajendragund2015@gmail.com>
2021-09-04 00:57:09 +0530

    Added : Program for Check Employee is present or absent

--- src/com/bridgelabz/EmpWageComp.java
+++ src/com/bridgelabz/EmpWageComp.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class EmpWageComp {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation Program !!");
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
